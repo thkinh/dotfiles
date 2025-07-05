e8cbe174d4cb4994a3e0a0daa45e53a265632c39
thkinh <139554989+thkinh@users.noreply.github.com.>
2025-07-06 00:02:12 +0700

    feat: tmux config added

--- install.sh
+++ install.sh
@@ -18,5 +18,7 @@ ln -sf "$DOTFILES_DIR/vim" "$HOME/.vim"
 # Git
 ln -sf "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
 
+#Tmux
+ln -sf "$DOTFILES_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "Done! You may want to reload your shell or restart the system."
